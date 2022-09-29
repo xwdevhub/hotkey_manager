d3fda1df746575eb362f7e0a8be98f4cc6b35b1f
15151866990@163.com <15151866990@163.com>
2022-09-29 19:42:32 +0800

    fix:修改common键点击弹出搜索问题

---
+++ lib/src/hotkey_manager.dart
@@ -0,0 +1,143 @@
+import 'dart:async';
+import 'package:collection/collection.dart';
+import 'package:flutter/services.dart';
+
+import './enums/key_code.dart';
+import './enums/key_modifier.dart';
+import './hotkey.dart';
+
+typedef HotKeyHandler = void Function(HotKey hotKey);
+
+class HotKeyManager {
+  HotKeyManager._();
+
+  /// The shared instance of [HotKeyManager].
+  static final HotKeyManager instance = HotKeyManager._();
+
+  final MethodChannel _channel = const MethodChannel('hotkey_manager');
+
+  bool _inited = false;
+  List<HotKey> _hotKeyList = [];
+  Map<String, HotKeyHandler> _keyDownHandlerMap = {};
+  Map<String, HotKeyHandler> _keyUpHandlerMap = {};
+
+  // 最后按下的快捷键
+  HotKey? _lastPressedHotKey;
+
+  void _init() {
+    RawKeyboard.instance.addListener(this._handleRawKeyEvent);
+    _channel.setMethodCallHandler(_methodCallHandler);
+    _inited = true;
+  }
+
+  _handleRawKeyEvent(RawKeyEvent value) {
+    if (value is RawKeyUpEvent && _lastPressedHotKey != null) {
+      HotKeyHandler? handler = _keyUpHandlerMap[_lastPressedHotKey!.identifier];
+      if (handler != null) handler(_lastPressedHotKey!);
+      _lastPressedHotKey = null;
+      return;
+    }
+
+    if (value is RawKeyDownEvent) {
+      if (value.repeat) return;
+      if (value.character == null) return;
+      HotKey? hotKey = _hotKeyList.firstWhereOrNull(
+        (e) {
+          return e.scope == HotKeyScope.inapp &&
+              value.isKeyPressed(e.keyCode.logicalKey) &&
+              value.data.modifiersPressed.keys.length ==
+                  (e.modifiers ?? []).length &&
+              (e.modifiers ?? []).every(
+                (m) => value.data.isModifierPressed(m.modifierKey),
+              );
+        },
+      );
+
+      if (hotKey != null) {
+        HotKeyHandler? handler = _keyDownHandlerMap[hotKey.identifier];
+        if (handler != null) handler(hotKey);
+        _lastPressedHotKey = hotKey;
+      }
+    }
+  }
+
+  Future<void> _methodCallHandler(MethodCall call) async {
+    String identifier = call.arguments['identifier'];
+    HotKey? hotKey = _hotKeyList.firstWhereOrNull(
+      (e) => e.identifier == identifier,
+    );
+    if (hotKey == null) {
+      print('[Warning] Can\'t find registered hotKey.');
+      return;
+    }
+
+    switch (call.method) {
+      case 'onKeyDown':
+        if (_keyDownHandlerMap.containsKey(identifier)) {
+          _keyDownHandlerMap[identifier]!(hotKey);
+        }
+        break;
+      case 'onKeyUp':
+        if (_keyUpHandlerMap.containsKey(identifier)) {
+          _keyUpHandlerMap[identifier]!(hotKey);
+        }
+        break;
+      default:
+        UnimplementedError();
+    }
+  }
+
+  List<HotKey> get registeredHotKeyList => _hotKeyList;
+
+  Future<void> register(
+    HotKey hotKey, {
+    HotKeyHandler? keyDownHandler,
+    HotKeyHandler? keyUpHandler,
+  }) async {
+    if (!_inited) this._init();
+
+    if (hotKey.scope == HotKeyScope.system) {
+      await _channel.invokeMethod('register', hotKey.toJson());
+    }
+    if (keyDownHandler != null)
+      _keyDownHandlerMap.update(
+        hotKey.identifier,
+        (_) => keyDownHandler,
+        ifAbsent: () => keyDownHandler,
+      );
+    if (keyUpHandler != null)
+      _keyUpHandlerMap.update(
+        hotKey.identifier,
+        (_) => keyUpHandler,
+        ifAbsent: () => keyUpHandler,
+      );
+
+    _hotKeyList.add(hotKey);
+  }
+
+  Future<void> unregister(HotKey hotKey) async {
+    if (!_inited) this._init();
+
+    if (hotKey.scope == HotKeyScope.system) {
+      await _channel.invokeMethod('unregister', hotKey.toJson());
+    }
+    if (_keyDownHandlerMap.containsKey(hotKey.identifier))
+      _keyDownHandlerMap.remove(hotKey.identifier);
+    if (_keyUpHandlerMap.containsKey(hotKey.identifier))
+      _keyUpHandlerMap.remove(hotKey.identifier);
+
+    _hotKeyList.removeWhere((e) => e.identifier == hotKey.identifier);
+  }
+
+  Future<void> unregisterAll() async {
+    if (!_inited) this._init();
+
+    await _channel.invokeMethod('unregisterAll');
+
+    _keyDownHandlerMap.clear();
+    _keyUpHandlerMap.clear();
+    _hotKeyList.clear();
+  }
+}
+
+final hotKeyManager = HotKeyManager.instance;
---
+++ lib/src/widgets/hotkey_recorder.dart
@@ -0,0 +1,90 @@
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../enums/key_code.dart';
+import '../enums/key_modifier.dart';
+import '../hotkey.dart';
+import './hotkey_virtual_view.dart';
+
+class HotKeyRecorder extends StatefulWidget {
+  final HotKey? initalHotKey;
+  final ValueChanged<HotKey> onHotKeyRecorded;
+
+  const HotKeyRecorder({
+    Key? key,
+    this.initalHotKey,
+    required this.onHotKeyRecorded,
+  }) : super(key: key);
+
+  @override
+  _HotKeyRecorderState createState() => _HotKeyRecorderState();
+}
+
+class _HotKeyRecorderState extends State<HotKeyRecorder> {
+  HotKey? _hotKey;
+
+  @override
+  void initState() {
+    if (widget.initalHotKey != null) {
+      _hotKey = widget.initalHotKey!;
+    }
+    RawKeyboard.instance.addListener(this._handleRawKeyEvent);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    RawKeyboard.instance.removeListener(this._handleRawKeyEvent);
+    super.dispose();
+  }
+
+  _handleRawKeyEvent(RawKeyEvent value) {
+    if (!(value is RawKeyDownEvent)) return;
+    if (value.character == null) return;
+
+    KeyCode? keyCode;
+    List<KeyModifier>? keyModifiers;
+
+    keyCode = KeyCode.values.firstWhereOrNull(
+      (kc) {
+        if (!value.isKeyPressed(kc.logicalKey)) return false;
+        KeyModifier? keyModifier =
+            KeyModifierParser.fromLogicalKey(kc.logicalKey);
+
+        if (keyModifier != null &&
+            value.data.isModifierPressed(keyModifier.modifierKey)) {
+          return false;
+        }
+
+        return true;
+      },
+    );
+    keyModifiers = KeyModifier.values
+        .where((km) => value.data.isModifierPressed(km.modifierKey))
+        .toList();
+
+    if (keyCode != null) {
+      _hotKey = HotKey(
+        keyCode,
+        modifiers: keyModifiers,
+      );
+      if (widget.initalHotKey != null) {
+        _hotKey?.identifier = widget.initalHotKey!.identifier;
+        _hotKey?.scope = widget.initalHotKey!.scope;
+      }
+
+      widget.onHotKeyRecorded(_hotKey!);
+
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_hotKey == null) {
+      return Container();
+    }
+    return HotKeyVirtualView(hotKey: _hotKey!);
+  }
+}
